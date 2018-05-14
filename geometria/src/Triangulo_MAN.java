ea98e7600b527e7d9547ba7731af59cb5149a4f0
marcos95an <marcos95an@gmail.com>
2018-05-14 20:59:56 +0200

    Versión comentada.

--- geometria/src/Triangulo_MAN.java
+++ geometria/src/Triangulo_MAN.java
@@ -1,9 +1,20 @@
-
+/**
+ * Esta es la clase triangulo.
+ * @author: Marcos Alcalá
+ * @version: 1.1
+ */
 public class Triangulo_MAN extends FiguraGeometrica_MAN {
 	private double lado1;
 	private double lado2;
 	private double lado3;
 	
+	/**
+	 * Constructor.
+     * @param tipoFigura
+     * @param lado1
+     * @param lado2
+     * @param lado3
+     */
 	public Triangulo_MAN(String tipoFigura, double lado1, double lado2, double lado3) {
 		super(tipoFigura);
 		lado1 = lado1;
@@ -11,11 +22,18 @@ public class Triangulo_MAN extends FiguraGeometrica_MAN {
 		lado3 = lado3;
 	}
 
+	/**
+	 * Método perimeto
+	 * @return la suma de los lados
+	 */
 	@Override
 	public double perimetro() {
 		return lado1 + lado2 + lado3;
 	}
 
+	/**
+	 * @return el área del triangulo
+	 */
 	@Override
 	public double area() { //Utiliza la formula de Her—n
 		double sp;
