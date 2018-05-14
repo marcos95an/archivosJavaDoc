07035697b8259db2ef29a8c5468072e700c6b68b
marcos95an <marcos95an@gmail.com>
2018-05-14 20:44:53 +0200

    Versión refactorizada

--- geometria/src/Circulo_MAN.java
+++ geometria/src/Circulo_MAN.java
@@ -1,10 +1,10 @@
 
-public class Circulo extends FiguraGeometrica {
+public class Circulo_MAN extends FiguraGeometrica_MAN {
 	private double radio;
 	//Constante pi
 	static final double PI = 3.1416;
 	
-	public Circulo(double r, String tipoFigura) {
+	public Circulo_MAN(double r, String tipoFigura) {
 		super(tipoFigura);
 		radio = r;
 	}
